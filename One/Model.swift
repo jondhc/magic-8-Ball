084846d7571709aa91860c53fb6f1257d7774ae1
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2017-11-14 00:41:29 -0600

    Rewritten according to Model - View

---
+++ One/Model.swift
@@ -0,0 +1,33 @@
+//
+//  Model.swift
+//  One
+//
+//  Created by Jonathan Daniel on 11/14/17.
+//  Copyright © 2017 Jonathan Herrejón. All rights reserved.
+//
+
+import Foundation
+
+class Model{
+    static let answers : [String] =
+        ["It is certain",
+         "It is decidedly so",
+         "Without a doubt",
+         "Yes definitely",
+         "You may rely on it",
+         "As I see it, yes",
+         "Most likely",
+         "Outlook good",
+         "Yes",
+         "Signs point to yes",
+         "Reply hazy try again",
+         "Ask again later",
+         "Better not tell you now",
+         "Cannot predict now",
+         "Concentrate and ask again",
+         "Don't count on it",
+         "My reply is no",
+         "My sources say no",
+         "Outlook not so good",
+         "Very doubtful"]
+}
--- One/ViewController.swift
+++ One/ViewController.swift
@@ -10,36 +10,6 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
    
-    // MODEL
-    //MARK: Values
-    let answers : [String] =
-        ["It is certain",
-         "It is decidedly so",
-         "Without a doubt",
-         "Yes definitely",
-         "You may rely on it",
-         "As I see it, yes",
-         "Most likely",
-         "Outlook good",
-         "Yes",
-         "Signs point to yes",
-         "Reply hazy try again",
-         "Ask again later",
-         "Better not tell you now",
-         "Cannot predict now",
-         "Concentrate and ask again",
-         "Don't count on it",
-         "My reply is no",
-         "My sources say no",
-         "Outlook not so good",
-         "Very doubtful"]
-    
-    
-    //////////////////////////
-    
-    
-    
-    
     //MARK: Properties
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var questionTextField: UITextField!
@@ -70,7 +40,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Actions
     @IBAction func askButton(_ sender: UIButton) {
         let answerNo = Int(arc4random_uniform(19))
-        answerLabel.text=answers[answerNo]
+        answerLabel.text=Model.answers[answerNo]
         questionTextField.text="";
         view.endEditing(true)
     }//end askButton
